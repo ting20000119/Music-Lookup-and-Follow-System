648f226316e63848a3580d457441558a56431c67
ting20000119 <jessica890119@gmail.com>
2022-01-02 01:03:21 +0800

    add songlist, searchArtist api

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import sqlite3
 from flask import Flask, render_template
-
+import json
 app = Flask(__name__)
+
 
 def get_db_connection():
     conn = sqlite3.connect('jam.db')
@@ -20,26 +21,68 @@ def index():
 @app.route('/register')
 def register():
     conn = get_db_connection()
-    posts = conn.execute('INSERT INTO MEMBER (MID,MNAME,MBIRTH,MGENDER,MCOUNTRY) VALUES (?,?,?,?,?)', (here))
+    posts = conn.execute(
+        'INSERT INTO MEMBER (MID,MNAME,MBIRTH,MGENDER,MCOUNTRY) VALUES (?,?,?,?,?)', (here))
     conn.commit()
     conn.close()
     return render_template('index.html', posts=posts)
 
+
+@app.route('/listSong/<string:mid>', methods=['GET'])
+def songList(mid):
+    conn = get_db_connection()
+    db = conn.cursor()
+    songs = db.execute(
+        'Select song.title,song.genre,song.year,song.language from song join add_list on song.sid = add_list.alsid where add_list.almid = ?', [mid]).fetchall()
+    conn.commit()
+    conn.close()
+    return app.response_class(response=json.dumps([dict(s) for s in songs]), status=200, mimetype='application/json')
+
+
+'''@app.route('/listArtist/<string:mid>', methods=['GET'])
+def artistList(mid):
+    
+
+@app.route('/listSongWriter/<string:mid>', methods=['GET'])
+def songWriterList(mid):
+    
+
+@app.route('/listProducer/<string:mid>', methods=['GET'])
+def producerList(mid):'''
+
+
+@app.route('/searchArtist/<string:aname>', methods=['GET'])
+def searchArtist(aname):
+    conn = get_db_connection()
+    db = conn.cursor()
+    songs = db.execute(
+        'Select song.title,song.genre,song.year,song.language from song join sing on song.sid = sing.ssid join artist on artist.aid = sing.said where artist.aname = ?', [aname]).fetchall()
+    conn.commit()
+    conn.close()
+    return app.response_class(response=json.dumps([dict(s) for s in songs]), status=200, mimetype='application/json')
+
+
 # Delete account
+
+
 def delete_account():
     conn = get_db_connection()
     enter_memID = input('enter member id: ')
-    posts = conn.execute('DELETE * FROM MEMBER WHERE MID = ?',(enter_memID))
+    posts = conn.execute('DELETE * FROM MEMBER WHERE MID = ?', (enter_memID))
     conn.commit()
     conn.close()
     return render_template('index.html', posts=posts)
 # login
+
+
 def login():
     conn = get_db_connection()
     enter_memID = input('enter member id: ')
-    posts = conn.execute('SELECT * FROM MEMBER WHERE MID = ?',(enter_memID))
+    posts = conn.execute('SELECT * FROM MEMBER WHERE MID = ?', (enter_memID))
     conn.commit()
     conn.close()
     return render_template('index.html', posts=posts)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
