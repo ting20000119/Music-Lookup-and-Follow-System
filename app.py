6e1b3694ea4764682c3ab402bb2ef77831a3591a
YR <45660085+storedspace@users.noreply.github.com>
2022-01-01 22:29:54 +0800

    Update app.py
    1. add 'delete account operation'
    2. add 'login operation'

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 def get_db_connection():
     conn = sqlite3.connect('jam.db')
     conn.row_factory = sqlite3.Row
@@ -21,12 +20,26 @@ def index():
 @app.route('/register')
 def register():
     conn = get_db_connection()
-    posts = conn.execute(
-        'INSERT INTO MEMBER (MID,MNAME,MBIRTH,MGENDER,MCOUNTRY) VALUES (?,?,?,?,?)', (here))
+    posts = conn.execute('INSERT INTO MEMBER (MID,MNAME,MBIRTH,MGENDER,MCOUNTRY) VALUES (?,?,?,?,?)', (here))
     conn.commit()
     conn.close()
     return render_template('index.html', posts=posts)
 
-
+# Delete account
+def delete_account():
+    conn = get_db_connection()
+    enter_memID = input('enter member id: ')
+    posts = conn.execute('DELETE * FROM MEMBER WHERE MID = ?',(enter_memID))
+    conn.commit()
+    conn.close()
+    return render_template('index.html', posts=posts)
+# login
+def login():
+    conn = get_db_connection()
+    enter_memID = input('enter member id: ')
+    posts = conn.execute('SELECT * FROM MEMBER WHERE MID = ?',(enter_memID))
+    conn.commit()
+    conn.close()
+    return render_template('index.html', posts=posts)
 if __name__ == '__main__':
     app.run(debug=True)
