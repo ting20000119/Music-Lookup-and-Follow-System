5c138c1edd2506b504a9c0398c11b3a1a56335c4
yako <ch995737@gmail.com>
2022-01-15 23:45:28 +0800

    1

--- frontend/login.js
+++ frontend/login.js
@@ -15,26 +15,26 @@ let login = async () => {
     };
     let Status = 0;
     let Message = "";
-    /*fetch 那裡放api*/ 
     var url = "http://127.0.0.1:5000/login/"+account.value;
     let res = await fetch(url, {
-        method: 'GET',
-        /*headers: {
-            "Content-type": "application/json",
-        },
-        body: JSON.stringify(payload),*/
+        method: 'GET'
     })
     .then((res) => {
-        console.log(res.json()) 
+        //res = res.json()
+        Status = res.status;
+        console.log("Status = "+Status);
         return res.json();
     })
     .catch((error) => {
         Message = "Something Wrong";
     });
     if (Status === 200 || Status === 201) {//成功
-        window.localStorage.setItem("UID", res.mid);
-        window.localStorage.setItem("name", res.mname);
-        window.location.href = "signup.html";
+        console.log("success");
+        
+        window.localStorage.setItem("UID", res[0].mid);
+        window.localStorage.setItem("name", res[0].mname);
+        console.log(window.localStorage.getItem("name"))
+        //window.location.href = "signup.html";
     } else {
         window.alert(res.message);
     }
